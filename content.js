d1e569a996f8bfb59702209fb55c16e77e1a9b53
Sebastiano Lewental <lewental.s@gmail.com>
2023-12-20 17:34:39 +0100

    Creato plugin per chrome che automatizza il login unipd

---
+++ content.js
@@ -0,0 +1,20 @@
+if (window.location.href === "https://stem.elearning.unipd.it/login/index.php") {
+    var ssoLogin = document.querySelector("#region-main > div > div > div > div > div > div.col-sm-12.col-lg-5.col-right > div.col-right-content > div:nth-child(5) > div.potentialidplist.mt-3 > div > a");
+
+    ssoLogin.style.background = "red";
+    ssoLogin.click();
+}
+
+var inputNome = document.getElementById("j_username_js");
+var inputPassword = document.getElementById("password");
+var radioBtn = document.getElementById("radio2");
+var loginBtn = document.getElementById("login_button_js");
+var loginForm = document.getElementById("user-login-form");
+
+setTimeout(() => {
+    radioBtn.checked = true;
+    inputNome.value = "sebastiano.lewental";
+    inputPassword.focus();
+    loginBtn.style.background = "red";
+    //loginBtn.click();
+}, 100);
